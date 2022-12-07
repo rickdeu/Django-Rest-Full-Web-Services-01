f8f79ca9cbd8eba586b54370ec984af74f648508
rickdeu <hangaloandre@gmail.com>
2022-12-07 13:56:33 +0100

    create views functions and urls

--- restfull01/urls.py
+++ restfull01/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from toys import urls as toy_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(toy_url)),
+
 ]
