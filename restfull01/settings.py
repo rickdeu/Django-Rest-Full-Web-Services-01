cfad89a96801c12e193e702e0ae2a2814dd21741
rickdeu <hangaloandre@gmail.com>
2022-12-20 10:37:35 +0100

    static files config

--- restfull01/settings.py
+++ restfull01/settings.py
@@ -120,9 +120,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = { 
+ 
+
+        'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' ,
+}
+CORS_ALLOW_ALL_ORIGINS = True
--- restfull01/urls.py
+++ restfull01/urls.py
@@ -1,27 +1,20 @@
-"""restfull01 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from toys import urls as toy_url
 from drones import urls as drone_url
+from rest_framework.documentation import include_docs_urls #api documentation
+
+API_TITLE = 'Drones API' # new
+API_DESCRIPTION = 'A Web API for Drones.'
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('toys/', include(toy_url)),
-    path('', include(drone_url)),
+    #path('toys/', include(toy_url)),
+    path('drones/', include(drone_url)),
+    path('', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+
 
 
 ]
